16fb53510fa3beb6d9405a7276eda395afa21847
EnzoReverdi <enzo.reverdi@epitech.eu>
2021-05-19 18:17:45 +0200

    added picom support

---
+++ scripts/picom/picom.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR=$(pwd)
+
+main () {
+    echo "##### PICOM ####"
+    rm -rf $HOME/.config/picom
+    if [[ -f $HOME/.config/picom.conf ]]
+    then
+        while true; do
+            read -p "Do you want to reinstall Picom ? (old config will be saved)[Y/n]" yn
+            case $yn in
+                [Yy]* ) move_oldconfig; break;;
+                [Nn]* ) exit 1;;
+                * ) echo "Please answer [y/n]";;
+            esac
+        done
+    fi
+    yay -S picom-ibhagwan-git
+    cp $DIR/config/picom.conf $HOME/.config/picom.conf
+}
+
+move_oldconfig () {
+    mv $HOME/.config/picom.conf $HOME/.config/picomOLD.conf
+}
+
+main "$@"
+
+# exec picom -b -f

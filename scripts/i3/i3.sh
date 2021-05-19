ff9cccfeb241e79b2c70b40436942a8ca5f99c04
alexniokiz <alexniokiz@gmail.com>
2021-05-20 01:12:53 +0200

    fix i3 feh

--- scripts/i3/i3.sh
+++ scripts/i3/i3.sh
@@ -6,11 +6,11 @@ main() {
     if [[ -f $HOME/.i3/config ]]; then
         mv $HOME/.i3/config $HOME/.i3/config.old
     fi
+    yes | yay -Sy brave feh playerctl
     cp -rf config/config $HOME/.i3/
     mkdir -p $HOME/.config/background
     cp -rf config/nord.jpg $HOME/.config/background
     feh --bg-scale $HOME/.config/background/nord.jpg
-    yes | yay -Sy brave feh playerctl
 }
 
 main "$@"

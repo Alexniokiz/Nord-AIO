a73efd5f6068bc9eaa4ebe2df4643c27a5e2ee92
EnzoReverdi <enzo.reverdi@epitech.eu>
2021-05-20 00:57:26 +0200

    fixed double compositor and startup performance for feh

--- scripts/i3/i3.sh
+++ scripts/i3/i3.sh
@@ -7,7 +7,9 @@ main() {
         mv $HOME/.i3/config $HOME/.i3/config.old
     fi
     cp -rf config/config $HOME/.i3/
-    cp -rf config/nord.jpg $HOME/Download
+    mkdir -p $HOME/.config/background
+    cp -rf config/nord.jpg $HOME/.config/background
+    feh --bg-scale $HOME/.config/background/nord.jpg
     yes | yay -Sy brave feh playerctl
 }
 

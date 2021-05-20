f46a7315c4e744d369a0427bb5ada3f01dc9eaf6
alexniokiz <alexniokiz@gmail.com>
2021-05-20 18:31:50 +0200

    extra packages install

---
+++ scripts/extra/extra.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+main() {
+    yay -Sy discord pavucontrol jq npm nodejs docker neofetch bashtop cpupower teams powertop cpupower-gui optimus-manager teamviewer slack-desktop
+}
+
+main "$@"

cb7ce99de0803259f2713f48163ccc30e8af6dd8
EnzoReverdi <enzo.reverdi@epitech.eu>
2021-05-20 15:34:27 +0200

    added Vim with a nord conf and some plugin

---
+++ scripts/editor/editor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+main () {
+    yay -Sy vim
+    rm -rf ~/.vimrc
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    cp $DIR/config/.vimrc ~/
+    while true; do
+        read -p "OPEN A NEW TERMINAL, START VIM, AND TYPE :PlugInstall , WAIT FOR IT TO FINISH AND COMEBACK HERE PRESSING Y OR N" yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) break;;
+            * ) echo "OPEN A NEW TERMINAL, START VIM, AND TYPE :PlugInstall , WAIT FOR IT TO FINISH AND COMEBACK HERE PRESSING Y OR N";;
+    esac
+}
+
+main "$@"
